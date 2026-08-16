74764192217ca8e2336e59bb051f04ab18c99579
Cartaz <prenc.francesco@gmail.com>
2026-08-16 18:49:09 +0200

    Load secondary character wiki data

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-const ASSET_VERSION = "20260816";
+const ASSET_VERSION = "20260816-25";
 
 async function loadJson(path) {
   const response = await fetch(`${path}?v=${ASSET_VERSION}`, { cache: "no-store" });
@@ -7,14 +7,15 @@ async function loadJson(path) {
 }
 
 export async function loadData() {
-  const [characters, locations, chapters, events, states, readerProgress, microWiki] = await Promise.all([
+  const [characters, locations, chapters, events, states, readerProgress, microWiki, characterWiki] = await Promise.all([
     loadJson("data/characters.json"),
     loadJson("data/locations.json"),
     loadJson("data/chapters.json"),
     loadJson("data/events.json"),
     loadJson("data/character-states.json"),
     loadJson("data/reader-progress.json"),
-    loadJson("data/context/micro-wiki.json")
+    loadJson("data/context/micro-wiki.json"),
+    loadJson("data/context/character-wiki.json")
   ]);
-  return { characters, locations, chapters, events, states, readerProgress, microWiki };
+  return { characters, locations, chapters, events, states, readerProgress, microWiki, characterWiki };
 }
